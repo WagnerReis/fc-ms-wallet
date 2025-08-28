0fe64851bc146b77f9e9126544da1d2a6c9b3a5b
Wagner Reis <wagnerreis1997@gmail.com>
2025-08-28 08:48:30 -0300

    feat: Enhance create transaction use case with event dispatching and add event dispatcher implementation

--- internal/usecase/create_transaction/create_transaction.go
+++ internal/usecase/create_transaction/create_transaction.go
@@ -3,6 +3,7 @@ package createtransaction
 import (
 	"github.com/WagnerReis/fc-ms-wallet/internal/entity"
 	"github.com/WagnerReis/fc-ms-wallet/internal/gateway"
+	"github.com/WagnerReis/fc-ms-wallet/pkg/events"
 )
 
 type CreateTransactionInputDTO struct {
@@ -18,12 +19,21 @@ type createtransactionOutputDTO struct {
 type CreateTransactionUseCase struct {
 	TransactionGateway gateway.TransactionGateway
 	AccountGateway     gateway.AccountGateway
+	EventDispatcher    events.EventDispatcherInterface
+	TransactionCreated events.EventInterface
 }
 
-func NewCreateTransactionUseCase(transactionGateway gateway.TransactionGateway, accountGateway gateway.AccountGateway) *CreateTransactionUseCase {
+func NewCreateTransactionUseCase(
+	transactionGateway gateway.TransactionGateway,
+	accountGateway gateway.AccountGateway,
+	eventDispatcher events.EventDispatcherInterface,
+	transactionCreated events.EventInterface,
+) *CreateTransactionUseCase {
 	return &CreateTransactionUseCase{
 		TransactionGateway: transactionGateway,
 		AccountGateway:     accountGateway,
+		EventDispatcher:    eventDispatcher,
+		TransactionCreated: transactionCreated,
 	}
 }
 
@@ -44,7 +54,13 @@ func (uc *CreateTransactionUseCase) Execute(input CreateTransactionInputDTO) (*c
 	if err != nil {
 		return nil, err
 	}
-	return &createtransactionOutputDTO{
+
+	output := &createtransactionOutputDTO{
 		ID: transaction.ID,
-	}, nil
+	}
+
+	uc.TransactionCreated.SetPayload(output)
+	uc.EventDispatcher.Dispatch(uc.TransactionCreated)
+
+	return output, nil
 }
---
+++ pkg/events/event_dispatcher.go
@@ -0,0 +1,60 @@
+package events
+
+import (
+	"errors"
+	"slices"
+)
+
+var ErrHandlerAlreadyRegistered = errors.New("handler already been registered")
+
+type EventDispatcher struct {
+	handlers map[string][]EventHandlerInterface
+}
+
+func NewEventDispatcher() *EventDispatcher {
+	return &EventDispatcher{
+		handlers: make(map[string][]EventHandlerInterface),
+	}
+}
+
+func (ed *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
+	if _, ok := ed.handlers[eventName]; ok {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				return ErrHandlerAlreadyRegistered
+			}
+		}
+	}
+	ed.handlers[eventName] = append(ed.handlers[eventName], handler)
+	return nil
+}
+
+func (ed *EventDispatcher) Clear() {
+	ed.handlers = make(map[string][]EventHandlerInterface)
+}
+
+func (ed *EventDispatcher) Has(eventName string, handler EventHandlerInterface) bool {
+	if handlers, ok := ed.handlers[eventName]; ok {
+		if slices.Contains(handlers, handler) {
+			return true
+		}
+	}
+	return false
+}
+
+func (ed *EventDispatcher) Dispatch(event EventInterface) error {
+	if handlers, ok := ed.handlers[event.GetName()]; ok {
+		for _, handler := range handlers {
+			go handler.Handle(event)
+		}
+	}
+	return nil
+}
+
+func (ed *EventDispatcher) Remove(eventName string, handler EventHandlerInterface) error {
+	if handlers, ok := ed.handlers[eventName]; ok {
+		index := slices.Index(handlers, handler)
+		ed.handlers[eventName] = append(handlers[:index], handlers[index+1:]...)
+	}
+	return nil
+}
