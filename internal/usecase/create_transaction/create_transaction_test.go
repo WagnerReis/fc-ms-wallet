2382491d008180db1f3c1d1e4c201a74601b1e44
Wagner Reis <wagnerreis1997@gmail.com>
2025-08-31 07:43:17 -0300

    feat: add UowMock for unit testing and refactor CreateTransactionUseCase to utilize Unit of Work pattern

--- internal/usecase/create_transaction/create_transaction_test.go
+++ internal/usecase/create_transaction/create_transaction_test.go
@@ -1,10 +1,12 @@
 package create_transaction
 
 import (
+	"context"
 	"testing"
 
 	"github.com/WagnerReis/fc-ms-wallet/internal/entity"
 	"github.com/WagnerReis/fc-ms-wallet/internal/event"
+	"github.com/WagnerReis/fc-ms-wallet/internal/usecase/mocks"
 	"github.com/WagnerReis/fc-ms-wallet/pkg/events"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -47,12 +49,8 @@ func TestCreateTransactionUseCase_Execute(t *testing.T) {
 	account2 := entity.NewAccount(client2)
 	account2.Credit(2000)
 
-	mockAccount := &AccountGatewayMock{}
-	mockAccount.On("FindByID", account1.ID).Return(account1, nil)
-	mockAccount.On("FindByID", account2.ID).Return(account2, nil)
-
-	mockTransaction := &TransactionGatewayMock{}
-	mockTransaction.On("Create", mock.Anything).Return(nil)
+	mockUow := &mocks.UowMock{}
+	mockUow.On("Do", mock.Anything, mock.Anything).Return(nil)
 
 	inputDto := CreateTransactionInputDTO{
 		AccountIDFrom: account1.ID,
@@ -62,13 +60,12 @@ func TestCreateTransactionUseCase_Execute(t *testing.T) {
 
 	dispatcher := events.NewEventDispatcher()
 	event := event.NewTransactionCreated()
+	ctx := context.Background()
 
-	uc := NewCreateTransactionUseCase(mockTransaction, mockAccount, *dispatcher, event)
-	outputDto, err := uc.Execute(inputDto)
+	uc := NewCreateTransactionUseCase(mockUow, *dispatcher, event)
+	outputDto, err := uc.Execute(ctx, inputDto)
 	assert.Nil(t, err)
 	assert.NotNil(t, outputDto)
-	mockAccount.AssertExpectations(t)
-	mockTransaction.AssertExpectations(t)
-	mockAccount.AssertNumberOfCalls(t, "FindByID", 2)
-	mockTransaction.AssertNumberOfCalls(t, "Create", 1)
+	mockUow.AssertExpectations(t)
+	mockUow.AssertNumberOfCalls(t, "Do", 1)
 }
