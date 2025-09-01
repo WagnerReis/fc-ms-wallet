052b1a56b4ec728528fb8b0b7f23ddd75406f8d0
Wagner Reis <wagnerreis1997@gmail.com>
2025-09-01 15:47:01 -0300

    feat: update CreateTransactionUseCase to include BalanceUpdated event for enhanced transaction handling

--- cmd/walletcore/main.go
+++ cmd/walletcore/main.go
@@ -37,6 +37,9 @@ func main() {
 	transactionCreatedEvent := event.NewTransactionCreated()
 	eventDispatcher.Register("TransactionCreated", handler.NewTransactionCreatedKafkaHandler(kafkaProducer))
 
+	balanceUpdatedEvent := event.NewBalanceUpdated()
+	eventDispatcher.Register("BalanceUpdated", handler.NewBalanceUpdatedKafkaHandler(kafkaProducer))
+
 	clientDb := database.NewClientDB(db)
 	accountDb := database.NewAccountDB(db)
 
@@ -53,7 +56,7 @@ func main() {
 
 	createClientUseCase := create_client.NewCreateClientUseCase(clientDb)
 	createAccountUseCase := create_account.NewCreateAccountUseCase(accountDb, clientDb)
-	createTransactionUseCase := create_transaction.NewCreateTransactionUseCase(uow, *eventDispatcher, transactionCreatedEvent)
+	createTransactionUseCase := create_transaction.NewCreateTransactionUseCase(uow, *eventDispatcher, transactionCreatedEvent, balanceUpdatedEvent)
 
 	webserver := webserver.NewWebServer(":8080")
 
